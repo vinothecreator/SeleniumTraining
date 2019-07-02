a7b36a7306b7f8547056fcf0eda690a8960a380a
vgunasekaran <vgunasekaran@VGUNASEKARAN.classic.pchad.com>
2019-07-02 18:57:35 +0530

    list 2 to list24

--- Training/src/test/java/collections/HashTable.java
+++ Training/src/test/java/collections/HashTable.java
@@ -26,11 +26,11 @@ public class HashTable {
 	//	File srcfile =((TakeScreenshot)driver).getScreenshotAs(outputType.FILE);
 		 List list = Arrays.asList("one Two three Four five six one three Four".split(" "));
 		 
-		 List<String> list2 = Arrays.asList("la la zslalalalalala".replaceAll("la", "vino"));
+		 List<String> list24= Arrays.asList("la la zslalalalalala".replaceAll("la", "vino"));
 		 
 		  List<Integer> list3 = Arrays.asList(5,4,3,7,2,1);
 		 Collections.sort(list3);
-		 //System.out.println(list2);
+		 //System.out.println(list24);
 		 list3.forEach(System.out:: println);
 		 
 		 
